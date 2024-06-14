f09790548059d04d6cd4ec4607482fc21bfdd8cf
phonglam <lamphong1507@gmail.com>
2024-06-14 13:05:55 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -123,7 +123,6 @@ export default function AppRoute() {
       <Route path={routes.notfound} element={<ErrorPage />} />
       <Route path={routes.cart} element={<CartPage />} />
       <Route path={routes.checkout} element={<CheckOut />} />
-      <Route path={routes.priceDiamond} element={<DiamondPricePage />} />
       <Route path={routes.productdetail} element={<ProductPage />} />
       <Route path={routes.saleEvent} element={<SaleEventPage />} />
       <Route path={routes.saleProduct} element={<SaleProductPage />} />
