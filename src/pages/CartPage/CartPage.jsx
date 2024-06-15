7755c8e503d6641a861ab33a263518d212616e3b
htqnhat280203 <htqnhat280203@gmail.com>
2024-06-15 12:59:20 +0700

    cart icon

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -69,6 +69,7 @@ export default function CartPage() {
     };
 
     return (
+<<<<<<< Updated upstream
         <div className="cart-page-container">
             <Header />
             <Container className="cart-container">
@@ -76,12 +77,22 @@ export default function CartPage() {
                     <Col md={8} className="cart-col8">
                         <h5><ImCart /> Giỏ hàng ({totalItems} sản phẩm)</h5>
                         <div className='cart-continue-btn'>
+=======
+        <div className="page-container">
+            <Header />
+            <Container className="container">
+                <Row>
+                    <Col md={8} className="Col8 col-md-8">
+                        <h4><ImCart /> Giỏ hàng ({totalItems} sản phẩm)</h4>
+                        <div className='continue-btn'>
+>>>>>>> Stashed changes
                             <Button variant="light" className="w-100 mt-2" type="button"><IoMdArrowRoundBack /> Tiếp tục mua hàng</Button>
                         </div>
 
                         <Card>
                             <ListGroup variant="flush">
                                 {cartItems.map(item => (
+<<<<<<< Updated upstream
                                     <ListGroup.Item key={item.id} className="cart-order-item">
                                         <div className="cart-product-details">
                                             <Image src={item.image} alt={item.name} className="cart-product-image" />
@@ -89,19 +100,37 @@ export default function CartPage() {
                                                 <h5>{item.name}</h5>
                                                 <p>MSP: {item.code}</p>
                                                 <div className="cart-quantity-control">
+=======
+                                    <ListGroup.Item key={item.id} className="order-item">
+                                        <div className="product-details">
+                                            <Image src={item.image} alt={item.name} className="product-image" />
+                                            <div className="order-item-details">
+                                                <h5>{item.name}</h5>
+                                                <p>MSP: {item.code}</p>
+                                                <div className="quantity-control">
+>>>>>>> Stashed changes
                                                     {/* <ButtonGroup>
                                                         <Button variant="outline-secondary" onClick={() => updateQuantity(item.id, -1)}>-</Button>
                                                         <span className="quantity">{item.quantity}</span>
                                                         <Button variant="outline-secondary" onClick={() => updateQuantity(item.id, 1)}>+</Button>
                                                     </ButtonGroup> */}
                                                 </div>
+<<<<<<< Updated upstream
                                                 <div className="cart-price-info">
                                                     <span className="cart-price-text">Giá tiền: <span style={{ color: 'red' }}>{item.price.toLocaleString()}đ</span></span>
+=======
+                                                <div className="price-info">
+                                                    <span className="price-text">Giá tiền: <span style={{ color: 'red' }}>{item.price.toLocaleString()}đ</span></span>
+>>>>>>> Stashed changes
                                                     <span>Tạm tính: <span style={{ color: 'red' }}>{(item.price * item.quantity).toLocaleString()}đ</span></span>
                                                 </div>
                                                 <span>Thành tiền: <span style={{ color: 'red' }}>{(item.price * item.quantity).toLocaleString()}đ</span></span>
                                                 <p>Mô tả: {item.description}</p>
+<<<<<<< Updated upstream
                                                 <span style={{ color: '#ce0303', cursor: 'pointer', textDecoration: 'underline' }} onClick={() => removeItem(item.id)} >Xóa</span>
+=======
+                                                <span style={{color: '#ce0303', cursor: 'pointer', textDecoration: 'underline'}} onClick={() => removeItem(item.id)} >Xóa</span>
+>>>>>>> Stashed changes
                                             </div>
                                         </div>
                                     </ListGroup.Item>
@@ -109,12 +138,18 @@ export default function CartPage() {
                             </ListGroup>
                         </Card>
                     </Col>
+<<<<<<< Updated upstream
                     <Col md={4} className="cart-col4">
                         <div className="cart-summary">
+=======
+                    <Col md={4} className="col-md-4">
+                        <div className="Col4">
+>>>>>>> Stashed changes
                             <Card>
                                 <Card.Header><h4>Tổng Tiền</h4></Card.Header>
                                 <Card.Body>
                                     <h5>Tạm tính: <span style={{ color: 'black', float: 'right' }}>{total.toLocaleString()} VNĐ</span></h5>
+<<<<<<< Updated upstream
                                     <hr className="cart-solid"></hr>
                                     <h5>Vận chuyển: <span style={{ color: 'black', float: 'right' }}>Miễn phí vận chuyển</span></h5>
                                     <hr className="cart-solid"></hr>
@@ -124,6 +159,17 @@ export default function CartPage() {
                                         <Button style={{ background: '#614A4A' }} className="cart-apply-button">Áp dụng</Button>
                                     </div>
                                     <Button style={{ background: '#ce0303', marginTop: '15px' }} className="w-100 cart-btn-proceed-to-checkout" type="submit">Tiến hành đặt hàng</Button>
+=======
+                                    <hr class="solid"></hr>
+                                    <h5>Vận chuyển: <span style={{ color: 'black', float: 'right' }}>Miễn phí vận chuyển</span></h5>
+                                    <hr class="solid"></hr>
+                                    <h5>Thanh toán: <span style={{ color: 'black', float: 'right' }}>{total.toLocaleString()} VNĐ</span></h5>
+                                    <div className="d-flex">
+                                        <input type="text" className="form-control mr-2" placeholder="Mã giảm giá/Quà tặng" />
+                                        <Button style={{ background: '#614A4A' }} className="apply-button">Áp dụng</Button>
+                                    </div>
+                                    <Button style={{ background: '#ce0303', marginTop: '15px' }} className="w-100 btn-proceed-to-checkout" type="submit">Tiến hành đặt hàng</Button>
+>>>>>>> Stashed changes
                                 </Card.Body>
                             </Card>
                         </div>
@@ -133,4 +179,8 @@ export default function CartPage() {
             <Footer />
         </div>
     );
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
