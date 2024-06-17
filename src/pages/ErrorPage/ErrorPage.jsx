e752b58eb58894b7eef2f5a229a16837e6669dae
htqnhat280203 <htqnhat280203@gmail.com>
2024-06-17 22:51:28 +0700

    Update error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -16,12 +16,12 @@ const ErrorPage = () => {
       />
       <script
         src="https://kit.fontawesome.com/4b9ba14b0f.js"
-        crossOrigin="anonymous"
+        crossOrigin="anonymous" 
       ></script>
 
       <div className="content">
         <div className="err">𝟒</div>
-        <img src={error} alt="Your Image" className="my-image" />
+        <img src="https://drive.google.com/thumbnail?id=1dvxc2GycD5DI3Esb-1LmMOF_TF2Yjnkv&sz=w1000" alt="Your Image" className="my-image" />
         <div className="err2">𝟒</div>
       </div>
       <div className="msg">
