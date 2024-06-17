3be425c14e090829c8e927a373d0fbc28acdda1a
htqnhat280203 <htqnhat280203@gmail.com>
2024-06-17 13:11:35 +0700

    Update products.js

--- src/components/searchBar/products.js
+++ src/components/searchBar/products.js
@@ -1,10 +1,10 @@
 // products.js
 export const products = [
     { id: 1, name: "Kim cương", description: "This is the first product.", price: 10.99 },
-    { id: 2, name: "Nhẫn", description: "This is the second product.", price: 20.99 },
+    { id: 2, name: "Kim cương 2", description: "This is the second product.", price: 20.99 },
     { id: 3, name: "Vòng", description: "This is the third product.", price: 30.99 },
     { id: 4, name: "Dây chuyền", description: "This is the fourth product.", price: 40.99 },
-    { id: 5, name: "Đá", description: "This is the fifth product.", price: 50.99 },
+    { id: 5, name: "Kim cương 3", description: "This is the fifth product.", price: 50.99 },
     { id: 6, name: "Hột", description: "This is the 6 ", price: 40.99 },
     { id: 7, name: "Vòng cổ", description: "This is the 7 product.", price: 50.99 },
     { id: 8, name: "Xích", description: "This is the 8 product.", price: 40.99 },
