9d86e19c8c20a60333cb4e755b253ff92564e8e6
phonglam <lamphong1507@gmail.com>
2024-06-21 02:03:08 +0700

    Update ProductDetailPage.jsx

--- src/pages/ProductPage/ProductDetailPage.jsx
+++ src/pages/ProductPage/ProductDetailPage.jsx
@@ -48,7 +48,6 @@ export default function ProductPage() {
         toast.error("Có lỗi trong lúc thêm sản phẩm");
       }
     } else {
-      z;
       toast.error("Bạn Chưa Chọn Size cho sản phẩm");
     }
   };
