283d7c496b8f1071693dbcb97ff357b55f313a08
htqnhat280203 <htqnhat280203@gmail.com>
2024-07-06 22:44:38 +0700

    update pagination for CollectionDetail.jsx

--- src/pages/CollectionPage/CollectionDetail.jsx
+++ src/pages/CollectionPage/CollectionDetail.jsx
@@ -6,11 +6,14 @@ import "./CollectionSet.css";
 import ProductCard from "../../components/productCard/productCard";
 import api from "../../config/axios";
 import { Link, useParams } from "react-router-dom";
+import Pagination from 'react-bootstrap/Pagination';
 
 function CollectionDetail() {
   const { id } = useParams();
   const [collection, setCollection] = useState(null); // Initialize as null
   const [product, setProduct] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 8;
 
   async function fetchCollectionById() {
     try {
@@ -43,7 +46,15 @@ function CollectionDetail() {
     (product) => product.collection?.id === collection?.id
   );
 
-  // Check loading or error states before rendering
+  // Pagination
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = productByCollection.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const totalPages = Math.ceil(productByCollection.length / productsPerPage);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   if (!collection) {
     return <div>Không có bộ sưu tập này</div>;
@@ -52,15 +63,14 @@ function CollectionDetail() {
   return (
     <div>
       <Header />
-
       <Container>
         <div className="set-banner">
           <img src={collection.imgURL} alt={collection.name} />
         </div>
         <h3 className="collection-set-header">{collection.name}</h3>
         <Row>
-          {productByCollection.map((product) => (
-            <Col key={product.id}>
+          {currentProducts.map((product, index) => (
+            <Col key={product.id} sm={6} md={3}>
               <Link to={`/chi-tiet-san-pham/${product.id}`}>
                 <ProductCard
                   img={product.imgURL}
@@ -73,6 +83,17 @@ function CollectionDetail() {
             </Col>
           ))}
         </Row>
+        <Pagination>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <Pagination.Item
+              key={index + 1}
+              active={index + 1 === currentPage}
+              onClick={() => paginate(index + 1)}
+            >
+              {index + 1}
+            </Pagination.Item>
+          ))}
+        </Pagination>
       </Container>
       <Footer />
     </div>
