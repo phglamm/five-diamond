f6967ee73b4586e2de2e411cf2ccef5a2d02ae18
phonglam <lamphong1507@gmail.com>
2024-07-21 21:38:12 +0700

    update diamond Price

--- src/pages/AdminDashboard/AdminDiamondPrice/AdminDiamondPrice.jsx
+++ src/pages/AdminDashboard/AdminDiamondPrice/AdminDiamondPrice.jsx
@@ -3,6 +3,7 @@ import { Button, Table, Input, Modal } from "antd";
 import { useEffect, useState } from "react";
 import "../../AdminDashboard/AdminPage.css";
 import axios from "axios";
+import api from "../../../config/axios";
 
 export default function AdminDiamondPrice() {
   const [price, setPrice] = useState([]);
@@ -10,9 +11,7 @@ export default function AdminDiamondPrice() {
   const [newPrice, setNewPrice] = useState("");
 
   async function fetchPrice() {
-    const response = await axios.get(
-      "https://6684dca756e7503d1ae169ba.mockapi.io/api/v1/DiamondPrice"
-    );
+    const response = await api.get("diamond-price");
     setPrice(response.data);
     console.log(response.data);
   }
@@ -21,24 +20,6 @@ export default function AdminDiamondPrice() {
     fetchPrice();
   }, []);
 
-  const updatePrice = async (values) => {
-    try {
-      const response = await axios.put(
-        `https://6684dca756e7503d1ae169ba.mockapi.io/api/v1/DiamondPrice/${values.DPId}`,
-        {
-          ...values,
-          price: newPrice,
-        }
-      );
-      console.log(response.data);
-      fetchPrice();
-      setEditingItem(null);
-      setNewPrice("");
-    } catch (error) {
-      console.error("Error updating price:", error);
-    }
-  };
-
   const onChange = (pagination, filters, sorter, extra) => {
     console.log("params", pagination, filters, sorter, extra);
   };
@@ -46,8 +27,8 @@ export default function AdminDiamondPrice() {
   const columns = [
     {
       title: "ID",
-      dataIndex: "DPId",
-      key: "DPId",
+      dataIndex: "id",
+      key: "id",
       sorter: (a, b) => a.id - b.id,
     },
     {
@@ -59,42 +40,17 @@ export default function AdminDiamondPrice() {
       title: "Giá",
       dataIndex: "price",
       key: "price",
-      render: (text, record) =>
-        editingItem === record.DPId ? (
-          <Input
-            value={newPrice}
-            onChange={(e) => setNewPrice(e.target.value)}
-          />
-        ) : (
-          parseFloat(text).toLocaleString()
-        ),
+      render: (text, record) => parseFloat(text).toLocaleString(),
     },
     {
       title: "Độ Tinh Khiết",
-      dataIndex: "cut",
-      key: "cut",
+      dataIndex: "clarity",
+      key: "clarity",
     },
     {
       title: "Màu",
       dataIndex: "color",
       key: "color",
-    },
-    {
-      title: "Hành động",
-      key: "action",
-      render: (values) =>
-        editingItem === values.DPId ? (
-          <Button onClick={() => updatePrice(values)}>Lưu</Button>
-        ) : (
-          <Button
-            onClick={() => {
-              setEditingItem(values.DPId);
-              setNewPrice(values.price);
-            }}
-          >
-            Cập nhật giá
-          </Button>
-        ),
     },
   ];
 
